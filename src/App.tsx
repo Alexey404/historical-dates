ca1aa37a3fa00f93409066d6aa2b0709fee3c759
Alexey <volik.aleksej@in-gir.ru>
2024-11-13 00:33:44 +0300

    Add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./app.scss";
 import { HistoricalDates } from "./components/HistoricalDates/HistoricalDates";
 import { histoticalData } from "./constants/histoticalData";
+import styles from "./app.module.scss";
 
 export const App: React.FC = () => {
   return (
-    <div className="app">
-      <div className="container">
+    <div className={styles["app"]}>
+      <div className={styles["app__container"]}>
         <HistoricalDates contentItems={histoticalData} />
         {/* <HistoricalDates contentItems={histoticalData} /> */}
       </div>
