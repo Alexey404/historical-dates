eacb7fbd2ddcef18efd0aad9dfe3b379bf369359
Alexey <volik.aleksej@in-gir.ru>
2024-11-13 11:41:29 +0300

    Add styles for swiper

--- src/components/SwiperWrapper/SwiperWrapper.tsx
+++ src/components/SwiperWrapper/SwiperWrapper.tsx
@@ -1,68 +1,43 @@
-import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
-import { Virtual } from "swiper/modules";
-
 import { FC, useEffect, useRef, useState } from "react";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
 import { ContentType } from "../../types/HistoticalDataTypes";
-import "./swiper-wrapper.scss";
+import styles from "./swiper-wrapper.module.scss";
+import { useDevice } from "../../hooks/useDevice";
 
 type SwiperWrapperProps = {
   items: ContentType[];
 };
 
 export const SwiperWrapper: FC<SwiperWrapperProps> = ({ items }) => {
+  const [spaceBetween, setSpaceBetween] = useState(80);
   const swiperRef = useRef<SwiperClass | null>(null);
-  const [slidesPerView, setSlidesPerView] = useState(1); // Стартовое значение - 1 слайд
-
-
-  const calculateSlidesPerView = () => {
-    const windowWidth = window.innerWidth;
-    // Пример для слайдов шириной 320px
-    const slides = Math.floor(windowWidth / 320); // Вычисляем количество слайдов, которое помещается на экране
-    setSlidesPerView(slides > 0 ? slides : 1); // Если на экране не помещается ни один слайд, показываем хотя бы 1
-  };
+  const device = useDevice();
 
   useEffect(() => {
-    calculateSlidesPerView(); // Вначале рассчитываем количество слайдов
-    window.addEventListener("resize", calculateSlidesPerView); // Обновляем на изменение размера экрана
+    setSpaceBetween(device === "mobile" || device === "tablet" ? 25 : 80);
+  }, [device]);
 
-    return () => {
-      window.removeEventListener("resize", calculateSlidesPerView); // Убираем слушатель при размонтировании компонента
-    };
-  }, []);
-
-  const onReachEnd = () => {
-    if (swiperRef.current) {
-      swiperRef.current.allowSlideNext = false; // Отключить прокрутку вперед
-    }
-  };
-
-  const onReachBeginning = () => {
-    if (swiperRef.current) {
-      swiperRef.current.allowSlidePrev = false; // Отключить прокрутку назад
-    }
-  };
-
-  const onSlideChange = () => {
-    if (swiperRef.current) {
-      // Включаем прокрутку снова, если не на границе
-      swiperRef.current.allowSlideNext = !swiperRef.current.isEnd;
-      swiperRef.current.allowSlidePrev = !swiperRef.current.isBeginning;
-    }
-  };
   return (
     <Swiper
       onSwiper={swiper => (swiperRef.current = swiper)}
-      spaceBetween={10}
-      slidesPerView={slidesPerView}
-      onReachEnd={onReachEnd}
-      onReachBeginning={onReachBeginning}
-      onSlideChange={onSlideChange}
+      slidesPerView="auto"
+      className={styles["swiper"]}
+      spaceBetween={spaceBetween}
+      wrapperClass={styles["swiper__wrapper"]}
       loop={false}
-      watchOverflow={true}
     >
       {items.map((slideContent, index) => (
-        <SwiperSlide key={slideContent.id} virtualIndex={index}>
-          {slideContent.content}
+        <SwiperSlide
+          key={slideContent.id}
+          virtualIndex={index}
+          className={styles["swiper__slide"]}
+        >
+          <div key={slideContent.id} className={styles["swiper__item"]}>
+            <span className={styles["swiper__date"]}>{slideContent.date}</span>
+            <span className={styles["swiper__content"]}>
+              {slideContent.content}
+            </span>
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>
